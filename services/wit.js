26b909c102de504e1037e2245ece8f5b5bda8709
staszekdev <staszekdev@gmail.com>
2016-09-01 14:34:28 +0200

    Update wit.js

--- services/wit.js
+++ services/wit.js
@@ -104,33 +104,24 @@ var firstEntityValue = function (entities, entity) {
 // }
 
 const actions = {
-  send({sessionId}, {text}) {
-    // Our bot has something to say!
-    // Let's retrieve the Facebook user whose session belongs to
-    const recipientId = sessions[sessionId].fbid;
-    if (recipientId) {
-      // Yay, we found our recipient!
-      // Let's forward our bot response to her.
-      // We return a promise to let our bot know when we're done sending
-      return fbMessage(recipientId, text)
-      .then(() => null)
-      .catch((err) => {
-        console.error(
-          'Oops! An error occurred while forwarding the response to',
-          recipientId,
-          ':',
-          err.stack || err
-        );
-      });
-    } else {
-      console.error('Oops! Couldn\'t find user for session:', sessionId);
-      // Giving the wheel back to our bot
-      return Promise.resolve()
-    }
+  send(request, response) {
+    const {sessionId, context, entities} = request;
+    const {text, quickreplies} = response;
+    return new Promise(function(resolve, reject) {
+      console.log('sending...', JSON.stringify(response));
+      return resolve();
+    });
   },
-  // You should implement your custom actions here
-  // See https://wit.ai/docs/quickstart
+  ['fetch-weather']({context, entities}) {
+    return new Promise(function(resolve, reject) {
+      // Here should go the api call, e.g.:
+      // context.forecast = apiCall(context.loc)
+      context.forecast = 'sunny';
+      return resolve(context);
+    });
+  },
 };
+
 
 // SETUP THE WIT.AI SERVICE
 var getWit = function () {
