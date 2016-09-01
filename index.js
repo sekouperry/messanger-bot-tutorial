e87ab700a9814a93093ff3ae5427d4c9dc267a18
staszekdev <staszekdev@gmail.com>
2016-09-01 13:10:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -53,14 +53,14 @@ app.post('/webhooks', function (req, res) {
 
   // res.sendStatus(200)
   
-  messaging_events = req.body.entry[0].messaging;
+  var messaging_events = req.body.entry[0].messaging;
   for (i=0; i<messaging_events.length; i++)
   {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
+    var event = req.body.entry[0].messaging[i];
+    var sender = event.sender.id;
     if( event.message && event.message.text )
     {
-      text = event.message.text;
+      var text = event.message.text;
       console.log(text);
     }
   }
