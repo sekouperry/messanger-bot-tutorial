ef1d00283ab0658a25180c488103d4a888fb9e76
staszekdev <staszekdev@gmail.com>
2016-09-01 13:41:05 +0200

    Update wit.js

--- services/wit.js
+++ services/wit.js
@@ -79,17 +79,17 @@ var actions = {
 	// list of functions Wit.ai can execute
 	['fetch-weather'](sessionId, context, cb) {
 		// Here we can place an API call to a weather service
-		// if (context.loc) {
-		// 	getWeather(context.loc)
-		// 		.then(function (forecast) {
-		// 			context.forecast = forecast || 'sunny'
-		// 		})
-		// 		.catch(function (err) {
-		// 			console.log(err)
-		// 		})
-		// }
-
-		context.forecast = 'Sunny'
+		if (context.loc) {
+			getWeather(context.loc)
+				.then(function (forecast) {
+					context.forecast = forecast || 'sunny'
+				})
+				.catch(function (err) {
+					console.log(err)
+				})
+		}
+		
+		// context.forecast = 'Sunny'
 
 		cb(context)
 	},
