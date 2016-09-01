e2f4dc04ed4f366e3b289e1d7de4e5373ebe5a8b
staszekdev <staszekdev@gmail.com>
2016-09-01 14:15:13 +0200

    Update wit.js

--- services/wit.js
+++ services/wit.js
@@ -19,89 +19,118 @@ var firstEntityValue = function (entities, entity) {
 }
 
 
-var actions = {
-	say (sessionId, context, message, cb) {
-		// Bot testing mode, run cb() and return
-		if (require.main === module) {
-			cb()
-			return
-		}
+// var actions = {
+// 	say (sessionId, context, message, cb) {
+// 		// Bot testing mode, run cb() and return
+// 		if (require.main === module) {
+// 			cb()
+// 			return
+// 		}
 
-		console.log('WIT WANTS TO TALK TO:', context._fbid_)
-		console.log('WIT HAS SOMETHING TO SAY:', message)
-		console.log('WIT HAS A CONTEXT:', context)
+// 		console.log('WIT WANTS TO TALK TO:', context._fbid_)
+// 		console.log('WIT HAS SOMETHING TO SAY:', message)
+// 		console.log('WIT HAS A CONTEXT:', context)
 
-		if (checkURL(message)) {
-			FB.newMessage(context._fbid_, message, true)
-		} else {
-			FB.newMessage(context._fbid_, message)
-		}
+// 		if (checkURL(message)) {
+// 			FB.newMessage(context._fbid_, message, true)
+// 		} else {
+// 			FB.newMessage(context._fbid_, message)
+// 		}
 
 		
-		cb()
+// 		cb()
 		
-	},
+// 	},
 
-	merge(sessionId, context, entities, message, cb) {
-		console.log("##########merge##########")
-		// Reset the weather story
-		delete context.forecast
+// 	merge(sessionId, context, entities, message, cb) {
+// 		console.log("##########merge##########")
+// 		// Reset the weather story
+// 		delete context.forecast
 
-		// Retrive the location entity and store it in the context field
-		var loc = firstEntityValue(entities, 'location')
-		if (loc) {
-			context.loc = loc
-		}
+// 		// Retrive the location entity and store it in the context field
+// 		var loc = firstEntityValue(entities, 'location')
+// 		if (loc) {
+// 			context.loc = loc
+// 		}
 
-		// Reset the cutepics story
-		delete context.pics
+// 		// Reset the cutepics story
+// 		delete context.pics
 
-		// Retrieve the category
-		var category = firstEntityValue(entities, 'category')
-		if (category) {
-			context.cat = category
-		}
+// 		// Retrieve the category
+// 		var category = firstEntityValue(entities, 'category')
+// 		if (category) {
+// 			context.cat = category
+// 		}
 
-		// Retrieve the sentiment
-		var sentiment = firstEntityValue(entities, 'sentiment')
-		if (sentiment) {
-			context.ack = sentiment === 'positive' ? 'Glad your liked it!' : 'Aww, that sucks.'
-		} else {
-			delete context.ack
-		}
+// 		// Retrieve the sentiment
+// 		var sentiment = firstEntityValue(entities, 'sentiment')
+// 		if (sentiment) {
+// 			context.ack = sentiment === 'positive' ? 'Glad your liked it!' : 'Aww, that sucks.'
+// 		} else {
+// 			delete context.ack
+// 		}
 
-		cb(context)
-	},
+// 		cb(context)
+// 	},
 
-	error(sessionId, context, error) {
-		console.log(error.message)
-	},
+// 	error(sessionId, context, error) {
+// 		console.log(error.message)
+// 	},
 
-	// list of functions Wit.ai can execute
-	['fetch-weather'](sessionId, context, cb) {
-		console.log("##########fetch-weather##########")
-		// Here we can place an API call to a weather service
-		// if (context.loc) {
-		// 	getWeather(context.loc)
-		// 		.then(function (forecast) {
-		// 			context.forecast = forecast || 'sunny'
-		// 		})
-		// 		.catch(function (err) {
-		// 			console.log(err)
-		// 		})
-		// }
+// 	// list of functions Wit.ai can execute
+// 	['fetch-weather'](sessionId, context, cb) {
+// 		console.log("##########fetch-weather##########")
+// 		// Here we can place an API call to a weather service
+// 		// if (context.loc) {
+// 		// 	getWeather(context.loc)
+// 		// 		.then(function (forecast) {
+// 		// 			context.forecast = forecast || 'sunny'
+// 		// 		})
+// 		// 		.catch(function (err) {
+// 		// 			console.log(err)
+// 		// 		})
+// 		// }
 		
-		context.forecast = 'Sunny'
-		cb(context)
-	},
+// 		context.forecast = 'Sunny'
+// 		cb(context)
+// 	},
 
-	['fetch-pics'](sessionId, context, cb) {
-		var wantedPics = allPics[context.cat || 'default']
-		context.pics = wantedPics[Math.floor(Math.random() * wantedPics.length)]
+// 	['fetch-pics'](sessionId, context, cb) {
+// 		var wantedPics = allPics[context.cat || 'default']
+// 		context.pics = wantedPics[Math.floor(Math.random() * wantedPics.length)]
 
-		cb(context)
-	},
-}
+// 		cb(context)
+// 	},
+// }
+
+const actions = {
+  send({sessionId}, {text}) {
+    // Our bot has something to say!
+    // Let's retrieve the Facebook user whose session belongs to
+    const recipientId = sessions[sessionId].fbid;
+    if (recipientId) {
+      // Yay, we found our recipient!
+      // Let's forward our bot response to her.
+      // We return a promise to let our bot know when we're done sending
+      return fbMessage(recipientId, text)
+      .then(() => null)
+      .catch((err) => {
+        console.error(
+          'Oops! An error occurred while forwarding the response to',
+          recipientId,
+          ':',
+          err.stack || err
+        );
+      });
+    } else {
+      console.error('Oops! Couldn\'t find user for session:', sessionId);
+      // Giving the wheel back to our bot
+      return Promise.resolve()
+    }
+  },
+  // You should implement your custom actions here
+  // See https://wit.ai/docs/quickstart
+};
 
 // SETUP THE WIT.AI SERVICE
 var getWit = function () {
